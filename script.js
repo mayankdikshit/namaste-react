ea93fda55a4823dabd9be6d0e7898a6cac760357
mayankdikshit <mayankdikshit711@gmail.com>
2025-04-27 12:41:58 +0530

    git-ignore updated

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-let divs = document.querySelectorAll(".box");
-
-
-for (div of divs) {
-    console.log(div.innerText)
-}
-
-divs[0].innerText = "new unique value 1"
-divs[1].innerText = "new unique value 2"
-divs[2].innerText = "new unique value 3"
-
+import React from "react";
+import ReactDOM from "react-dom"
