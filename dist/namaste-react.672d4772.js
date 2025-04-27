7b40442e613bc7775a90e55a50f45ee69cd4ecad
mayankdikshit <mayankdikshit711@gmail.com>
2025-04-27 11:39:10 +0530

    react added

---
+++ dist/namaste-react.672d4772.js
@@ -0,0 +1,7 @@
+let divs = document.querySelectorAll(".box");
+for (div of divs)console.log(div.innerText);
+divs[0].innerText = "new unique value 1";
+divs[1].innerText = "new unique value 2";
+divs[2].innerText = "new unique value 3";
+
+//# sourceMappingURL=namaste-react.672d4772.js.map
